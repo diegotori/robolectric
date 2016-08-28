d6715b8132a261bbdf07e46d8d14ac9ce0f77a59
Diego Tori <diegotoridoesandroid@gmail.com>
2016-08-28 19:55:45 -0400

    Added new tests for previous ShadowGoogleApiClient changes.

--- robolectric-shadows/shadows-play-services/src/test/java/org/robolectric/shadows/gms/common/api/ShadowGoogleApiClientTest.java
+++ robolectric-shadows/shadows-play-services/src/test/java/org/robolectric/shadows/gms/common/api/ShadowGoogleApiClientTest.java
@@ -3,14 +3,20 @@ package org.robolectric.shadows.gms.common.api;
 import android.content.Context;
 import android.os.Looper;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.common.internal.zzf;
+import java.util.concurrent.TimeUnit;
+import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
@@ -24,6 +30,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -124,12 +132,377 @@ public class ShadowGoogleApiClientTest {
 
     @Test
     public void setConnectionResultForApi() {
-        final ConnectionResult expected = new ConnectionResult(ConnectionResult.TIMEOUT, null);
+        final ConnectionResult expected = new ConnectionResult(ConnectionResult.TIMEOUT);
         shadowGoogleApiClient.setConnectionResultForApi(mockApi, expected);
 
         final ConnectionResult actual = shadowGoogleApiClient.getConnectionResult(mockApi);
 
         assertThat(actual)
                 .isNotNull().isEqualTo(expected);
+    }
+
+    @Test
+    public void setHasConnectedApi() {
+        shadowGoogleApiClient.setHasConnectedApi(mockApi, true);
+
+        final boolean actual = shadowGoogleApiClient.hasConnectedApi(mockApi);
+
+        assertThat(actual)
+            .isTrue();
+    }
+
+    @Test
+    public void setHasConnectedApi__NullBoolean() {
+        shadowGoogleApiClient.setHasConnectedApi(mockApi, null);
+
+        final boolean actual = shadowGoogleApiClient.hasConnectedApi(mockApi);
+
+        assertThat(actual)
+            .isFalse();
+    }
+
+    @Test
+    public void setHasConnectedApi__False() {
+        shadowGoogleApiClient.setHasConnectedApi(mockApi, false);
+
+        final boolean actual = shadowGoogleApiClient.hasConnectedApi(mockApi);
+
+        assertThat(actual)
+            .isFalse();
+    }
+
+    @Test
+    public void setLooper() {
+        shadowGoogleApiClient.setLooper(mockLooper);
+
+        final Looper actual = shadowGoogleApiClient.getLooper();
+
+        assertThat(actual)
+                .isNotNull()
+                .isEqualTo(mockLooper);
+    }
+
+    @Test
+    public void setContext() {
+        shadowGoogleApiClient.setContext(mockContext);
+
+        final Context actual = shadowGoogleApiClient.getContext();
+
+        assertThat(actual)
+            .isNotNull()
+            .isEqualTo(mockContext);
+    }
+
+    @Test
+    public void registerConnectionCallbacks() {
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        final boolean actual = shadowGoogleApiClient.isConnectionCallbacksRegistered
+            (mockConnCallbacks);
+
+        assertThat(actual)
+            .isTrue();
+    }
+
+    @Test
+    public void unregisterConnectionCallbacks() {
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+        shadowGoogleApiClient.unregisterConnectionCallbacks(mockConnCallbacks);
+
+        final boolean actual = shadowGoogleApiClient.isConnectionCallbacksRegistered
+            (mockConnCallbacks);
+
+        assertThat(actual)
+            .isFalse();
+
+    }
+
+    @Test
+    public void registerConnectionFailedListener() {
+        shadowGoogleApiClient.registerConnectionFailedListener(mockConnFailedListener);
+
+        final boolean actual = shadowGoogleApiClient.isConnectionFailedListenerRegistered
+            (mockConnFailedListener);
+
+        assertThat(actual)
+            .isTrue();
+    }
+
+    @Test
+    public void unregisterConnectionFailedListener() {
+        shadowGoogleApiClient.registerConnectionFailedListener(mockConnFailedListener);
+        shadowGoogleApiClient.unregisterConnectionFailedListener(mockConnFailedListener);
+
+        final boolean actual = shadowGoogleApiClient.isConnectionFailedListenerRegistered
+            (mockConnFailedListener);
+
+        assertThat(actual)
+            .isFalse();
+    }
+
+    @Test
+    public void connect__ConnectionResult__Success() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        shadowGoogleApiClient.connect();
+
+        verify(mockConnCallbacks).onConnected(null);
+    }
+
+    @Test
+    public void connect__ConnectionResult__Success__NullConnectionCallbacks() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+        shadowGoogleApiClient.registerConnectionCallbacks(null);
+
+        try {
+            shadowGoogleApiClient.connect();
+        } catch (Exception e) {
+            Assertions.fail("Caught while trying to call back to a null ConnectionCallbacks "
+                + "instance: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void connect__ConnectionResult__NetworkError() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.NETWORK_ERROR);
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        shadowGoogleApiClient.connect();
+
+        verify(mockConnCallbacks)
+            .onConnectionSuspended(GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST);
+    }
+
+    @Test
+    public void connect__ConnectionResult__NetworkError__NullConnectionCallbacks() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.NETWORK_ERROR);
+        shadowGoogleApiClient.registerConnectionCallbacks(null);
+
+        try {
+            shadowGoogleApiClient.connect();
+        } catch (Exception e) {
+            Assertions.fail("Caught while trying to call back to a null ConnectionCallbacks "
+                + "instance: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void connect__ConnectionResult__Interrupted() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.INTERRUPTED);
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        shadowGoogleApiClient.connect();
+
+        verify(mockConnCallbacks)
+            .onConnectionSuspended(GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED);
+    }
+
+    @Test
+    public void connect__ConnectionResult__Interrupted__NullConnectionCallbacks() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.INTERRUPTED);
+        shadowGoogleApiClient.registerConnectionCallbacks(null);
+
+        try {
+            shadowGoogleApiClient.connect();
+        } catch (Exception e) {
+            Assertions.fail("Caught while trying to call back to a null ConnectionCallbacks "
+                + "instance: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void connect__ConnectionResult__AllOtherErrorCodes() {
+        final ArgumentCaptor<ConnectionResult> connResultCaptor = ArgumentCaptor.forClass
+            (ConnectionResult.class);
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.API_UNAVAILABLE);
+        shadowGoogleApiClient.registerConnectionFailedListener(mockConnFailedListener);
+
+        shadowGoogleApiClient.connect();
+
+        verify(mockConnFailedListener)
+            .onConnectionFailed(connResultCaptor.capture());
+        final ConnectionResult capturedConnResult = connResultCaptor.getValue();
+        assertThat(capturedConnResult)
+            .isNotNull();
+        assertThat(capturedConnResult.getErrorCode())
+            .isNotEqualTo(ConnectionResult.SUCCESS)
+            .isEqualTo(ConnectionResult.API_UNAVAILABLE);
+    }
+
+    @Test
+    public void connect__ConnectionResult__AllOtherErrorCodes__NullConnectionCallbacks() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.API_UNAVAILABLE);
+        shadowGoogleApiClient.registerConnectionFailedListener(null);
+
+        try {
+            shadowGoogleApiClient.connect();
+        } catch (Exception e) {
+            Assertions.fail("Caught while trying to call back to a null ConnectionCallbacks "
+                + "instance: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void connect__SignInModeRequired() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        shadowGoogleApiClient.connect(GoogleApiClient.SIGN_IN_MODE_REQUIRED);
+
+        verify(mockConnCallbacks).onConnected(null);
+        assertThat(shadowGoogleApiClient.getCurrentSignInMode())
+            .isNotNull()
+            .isEqualTo(GoogleApiClient.SIGN_IN_MODE_REQUIRED);
+    }
+
+    @Test
+    public void connect__SignInModeOptional() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        shadowGoogleApiClient.connect(GoogleApiClient.SIGN_IN_MODE_OPTIONAL);
+
+        verify(mockConnCallbacks).onConnected(null);
+        assertThat(shadowGoogleApiClient.getCurrentSignInMode())
+            .isNotNull()
+            .isEqualTo(GoogleApiClient.SIGN_IN_MODE_OPTIONAL);
+    }
+
+    @Test
+    public void connect__SignInModeNone() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        shadowGoogleApiClient.connect(3);
+
+        verify(mockConnCallbacks).onConnected(null);
+        assertThat(shadowGoogleApiClient.getCurrentSignInMode())
+            .isNotNull()
+            .isEqualTo(3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void connect__SignInModeInvalidValue() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+
+        shadowGoogleApiClient.connect(0);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void connect__SignInMode__CalledWithDifferentValue() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+        shadowGoogleApiClient.connect(GoogleApiClient.SIGN_IN_MODE_REQUIRED);
+
+        shadowGoogleApiClient.connect(GoogleApiClient.SIGN_IN_MODE_OPTIONAL);
+    }
+
+    @Test
+    public void disconnect() {
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        shadowGoogleApiClient.disconnect();
+
+        verify(mockConnCallbacks)
+            .onConnectionSuspended(GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED);
+    }
+
+    @Test
+    public void disconnect__NullConnectionCallbacks() {
+        shadowGoogleApiClient.registerConnectionCallbacks(null);
+
+        try {
+            shadowGoogleApiClient.disconnect();
+        } catch (Exception e) {
+            Assertions.fail("Caught while trying to call back to a null ConnectionCallbacks "
+                + "instance while disconnecting: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void reconnect() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        shadowGoogleApiClient.reconnect();
+
+        verify(mockConnCallbacks).onConnected(null);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void clearDefaultAccountAndReconnect() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+        final PendingResult<Status> mockPendingResult = mock(PendingResult.class);
+        shadowGoogleApiClient.setReconnectPendingResult(mockPendingResult);
+        shadowGoogleApiClient.registerConnectionCallbacks(mockConnCallbacks);
+
+        final PendingResult<Status> actual = shadowGoogleApiClient
+            .clearDefaultAccountAndReconnect();
+
+        verify(mockConnCallbacks)
+            .onConnectionSuspended(GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED);
+        verify(mockConnCallbacks).onConnected(null);
+        assertThat(actual)
+            .isNotNull()
+            .isEqualTo(mockPendingResult);
+    }
+
+    @Test
+    public void blockingConnect() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+
+        final ConnectionResult actual = shadowGoogleApiClient.blockingConnect();
+
+        assertThat(actual)
+            .isNotNull();
+        assertThat(actual.getErrorCode())
+            .isEqualTo(ConnectionResult.SUCCESS);
+    }
+
+    @Test
+    public void blockingConnect__WithTimeout() {
+        final ShadowGoogleApiAvailability shadowGoogleApiAvailability
+            = Shadows.shadowOf(GoogleApiAvailability.getInstance());
+        shadowGoogleApiAvailability.setIsGooglePlayServicesAvailable(ConnectionResult.SUCCESS);
+
+        final ConnectionResult actual = shadowGoogleApiClient.blockingConnect(10, TimeUnit.SECONDS);
+
+        assertThat(actual)
+            .isNotNull();
+        assertThat(actual.getErrorCode())
+            .isEqualTo(ConnectionResult.SUCCESS);
     }
 }
